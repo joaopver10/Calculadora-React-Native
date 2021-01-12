30d9561dca4c4bc8e8beceabac314162db0f6b7c
João Pedro <joaovitorino@unigranrio.br>
2021-01-11 23:25:40 -0300

    botão e display adicionado

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Button from './src/components/Button'
+import Display from './src/components/Display'
+
+export default class App extends Component {
+  state={
+    displayValue: '0'
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label='AC'/>
+          <Button label='/'/>
+          <Button label='7'/>
+          <Button label='8'/>
+          <Button label='9'/>
+          <Button label='*'/>
+          <Button label='4'/>
+          <Button label='5'/>
+          <Button label='6'/>
+          <Button label='-'/>
+          <Button label='1'/>
+          <Button label='2'/>
+          <Button label='3'/>
+          <Button label='+'/>
+          <Button label='0'/>
+          <Button label='.'/>
+          <Button label='='/>
+        </View>
+        <StatusBar hidden={true} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttons:{
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
 });
